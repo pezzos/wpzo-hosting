42a610420d4e0d73cd8b10f8169ff71c118fd14a
a.pezzotta <a.pezzotta@criteo.com>
2021-09-01 05:32:19 +0200

    Update nginx conf

--- scripts/install-site.sh
+++ scripts/install-site.sh
@@ -1,13 +1,70 @@
 #!/bin/bash
+RED='\033[31m'
+GREEN='\033[32m'
+NC='\033[0;39m' # No Color
 
-create host nginx
-create php fpm pool
-create dir domain/sub/www
-create logs
-composer wordpress
+function generateDirs {
+  mkdir -p /pezzos/servers/${DOMAIN}/${SUB}/conf/
+  mkdir -p /pezzos/servers/${DOMAIN}/${SUB}/web/
+  mkdir -p /pezzos/servers/${DOMAIN}/${SUB}/php/
+}
 
-problem droit user db all table
-CREATE USER 'pezzos'@'localhost' IDENTIFIED BY '69!!PezMar!!69';
-GRANT ALL PRIVILEGES ON *.* TO 'pezzos'@'localhost' WITH GRANT OPTION;
-CREATE USER 'pezzos'@'%' IDENTIFIED BY '69!!PezMar!!69';
-GRANT ALL PRIVILEGES ON *.* TO 'pezzos'@'%' WITH GRANT OPTION;
+function generatePhpFpmConf {
+  envsubst < ../php-fpm.d/conf.template > ../php-fpm.d/${TECHNAME}.conf
+  # envsubst < /pezzos/work/php-fpm.d/conf.template > /pezzos/work/php-fpm.d/${TECHNAME}.conf
+}
+
+function reloadServerPhpFpm {
+  systemctl reload php-fpm.service
+  ps aux | grep php-fpm | grep -v grep | grep -v efs  | grep ${TECHNAME} 2&1>/dev/null
+  if [ $? == 1 ]
+  then
+    printf "${RED}[err] Test: PHP FPM doesn't load the new pool ${TECHNAME}${NC}\n"
+    exit 1
+  fi
+  systemctl status php-fpm | grep running 2&1>/dev/null
+  if [ $? == 1 ]
+  then
+    printf "${RED}[err] Test: PHP FPM is not running${NC}\n"
+    exit 1
+  fi
+}
+
+function testServerPhpFpm {
+  php-fpm -tt |& grep "configuration file /etc/php-fpm.conf test is successful" 2&1>/dev/null
+  if [ $? == 1 ]
+  then
+    printf "${RED}[err] Test: PHP FPM configuration is broken${NC}\n"
+    exit 1
+  fi
+}
+
+if [[ -n $1 && -n $2 ]]
+then
+  DOMAIN=$1
+  DOMAIN_STRIP=$(echo $1 | tr -d '.')
+  SUB=$2
+  TECHNAME=${DOMAIN_STRIP}_${SUB}
+
+  export DOMAIN SUB TECHNAME
+  # readPass
+  printf "${NC}Generate directory structure${NC}\n"
+  # generateDirs
+    printf "${NC}Generate php fpm configuration${NC}\n"
+  # generatePhpFpmConf
+  testServerPhpFpm
+  reloadServerPhpFpm
+  testServerPhpFpm
+
+
+  # [ $? == 1 ] && exit 1
+  exit 0
+else
+  printf "${RED}[err] Syntax: ${0} dom.tld subdom${NC}\n"
+  exit 1
+fi
+
+# create host nginx
+# r
+# create logs
+# composer wordpress
